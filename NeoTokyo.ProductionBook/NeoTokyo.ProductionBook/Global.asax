af42c99d600c47e6eb132c78c1660383a231c3e1
rowley-pete <rowley.pete@live.co.uk>
2015-12-21 14:06:17 +0000

    First Commit

---
+++ NeoTokyo.ProductionBook/NeoTokyo.ProductionBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NeoTokyo.ProductionBook.MvcApplication" Language="C#" %>
---
+++ NeoTokyo.ProductionBook/NeoTokyo.ProductionBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NeoTokyo.ProductionBook.Startup))]
+namespace NeoTokyo.ProductionBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
